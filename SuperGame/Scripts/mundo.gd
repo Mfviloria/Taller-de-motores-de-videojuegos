d810392b5af2499296be73a034516aee297e2729
Mfviloria <mfviloria@uninorte.edu.co>
2026-04-08 17:36:28 -0500

    Sesión 2

---
+++ SuperGame/Scripts/mundo.gd
@@ -0,0 +1,5 @@
+extends Node2D
+
+
+func _on_area_2d_body_entered(body: CharacterBody2D) -> void:
+	print("Gustavo, David y Eduardo estan haciendo su primer juego")
--- SuperGame/Scripts/player.gd
+++ SuperGame/Scripts/player.gd
@@ -1,25 +1,20 @@
 extends CharacterBody2D
 var speed = 600
 var last_direction = ""
-@onready var player = $Sprite2D
+@onready var player = $AnimatedSprite2D
 func _physics_process(delta: float) -> void:
 	var directions = Input.get_vector("Izquierda", "Derecha", "Arriba", "Abajo")
 	velocity = directions * speed
+	
 	if directions == Vector2.ZERO:
 		directions = Vector2.ZERO
 		player.play("idle")
 	else:
-		if directions.x < 0:
-			player.play("left")
-		elif directions.x > 0:
-			player.play("right")
-		elif directions.y > 0:
-			player.play("down")
-		elif directions.y < 0 :
-			player.play("up")
-			
-				
-		
-		
-		
+		if directions.x > 0:
+			player.play("Derecha")
+		elif directions.x < 0:
+			player.play("Izquierda")
+		elif directions.y < 0:
+			player.play("Arriba")
+		else: player.play("Abajo")
 	move_and_slide()
